6b7529c6f36fca5623342a54e230f772112e4f62
John Z. Li <lizhao.johnlee@gmail.com>
2026-05-24 10:00:27 +0800

    remove outdated plugins for nvim0.12

--- init.lua
+++ init.lua
@@ -24,26 +24,39 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- add your plugins here
+    -- my plugins
+    -- corlorscheme
     { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+    -- some useful icons
     { "nvim-tree/nvim-web-devicons", opts = {} },
+    -- fuzzy finder
     { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
+    -- auto-completion
     { 'hrsh7th/nvim-cmp',  dependencies = { 'hrsh7th/cmp-nvim-lsp', 
                                             'hrsh7th/cmp-buffer',
 				            'hrsh7th/cmp-path',
 				            'hrsh7th/cmp-cmdline',
 				            'hrsh7th/cmp-vsnip',
 				            'hrsh7th/vim-vsnip'} },
+   -- install language servers
    { "mason-org/mason.nvim", opts = {} },
+   -- file system viewer
    { "nvim-tree/nvim-tree.lua", version = "*", lazy = false, dependencies = { "nvim-tree/nvim-web-devicons", }, },
+   -- enhanced status line
    { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' }, },
+   -- debug support
    { 'mfussenegger/nvim-dap', },
+   -- async task runner
    { 'stevearc/overseer.nvim',},
+   -- git wrapper
    { "NeogitOrg/neogit", dependencies = { "nvim-lua/plenary.nvim", 
                                           "sindrets/diffview.nvim", 
 					  "nvim-telescope/telescope.nvim", }, },
+   -- collect location list, quick fix list, etc
    { "folke/trouble.nvim", opts = {}, cmd = "Trouble", },
+   -- git integration to buffer
    { "lewis6991/gitsigns.nvim" },
+   -- formatter
    { 'mhartington/formatter.nvim' },
   },
   -- Configure any other settings here. See the documentation for more details.
